fa69e2568ec63d06352d0cc250af3545b74a7f4e
MinWu <wumin199@126.com>
2023-08-08 22:32:44 +0800

    update

--- Book4_Ch04_Python_Codes/Streamlit_Bk4_Ch4_16.py
+++ Book4_Ch04_Python_Codes/Streamlit_Bk4_Ch4_16.py
@@ -28,7 +28,6 @@ def bmatrix(a):
 
 n = m = 20
 
-
 # 最后，通过 fig.add_trace 函数将一个 Scatter 类型的图表对象添加到子图布局的第一个子图中。
 #
 # 该 Scatter 图表对象的 x 坐标使用 xv 列表，y 坐标使用 yv 列表。图表对象的 mode 设置为 "lines" 表示要绘制连续的线段，线宽使用变量 lw 指定，线的颜色设置为红色。
@@ -46,9 +45,22 @@ for k in range(-n, n + 1):
     # 生成的数据点序列中的 NaN 值可以用来分隔不同的曲线段或路径段
     xv.extend([k, k, np.nan])
     yv.extend([-m, m, np.nan])
+
+# print("xv: ", xv)
+# print("yv: ", yv)
+
 lw = 1  # line_width
+
+
+# go.Scatter 是 Plotly 库中的一个函数，用于创建 Scatter 类型的图表对象。
+
+# Scatter 类型图表对象用于绘制散点图、线图或线段等可视化图形。可以通过指定不同的模式（mode）参数来控制 Scatter 图表对象的绘制方式。
+
 fig.add_trace(
-    go.Scatter(x=xv, y=yv, mode="lines", line_width=lw, line_color="red"), 1, 1
+    # 有n个点，每个点的坐标是(xv[n], yv[n])，然后每个点都连线起来，如果一个点是[np.nan, np.nan]，则和前后点不连起来
+    go.Scatter(x=xv, y=yv, mode="lines", line_width=lw, line_color="red"),
+    1,
+    1,
 )
 # set up  the lists  of  horizontal line x and y-end coordinates
 
@@ -77,6 +89,7 @@ with st.sidebar:
     c = st.slider("c", -2.0, 2.0, step=0.1, value=0.0)
     d = st.slider("c", -2.0, 2.0, step=0.1, value=1.0)
 
+
 theta = np.pi / 6
 A = np.array([[a, b], [c, d]], dtype=float)
 
@@ -84,19 +97,35 @@ A = np.array([[a, b], [c, d]], dtype=float)
 # X = np.array(xv[6:-6])
 # Y = np.array(yv[6:-6])
 
+print("---")
+
+print("xv: ", xv)
+print("yv: ", yv)
+
 X = np.array(xv)
 Y = np.array(yv)
+
+# np.stack((X, Y))是垂直方向的网格线
+# Txvyv就是被A变换过的新的网格线
 
 # transform by T the vector of coordinates [x, y]^T where the vector runs over the columns of np.stack((X, Y))
 Txvyv = A @ np.stack((X, Y))  # transform by T the vertical lines
 
+
+print("Txvyv: ", Txvyv)
+
 # X = np.array(xh[6:-6])
 # Y = np.array(yh[6:-6])
+
 
 X = np.array(xh)
 Y = np.array(yh)
 
 Txhyh = A @ np.stack((X, Y))  # #transform by T the horizontal lines
+
+# np.stack((X, Y))是水平方向的网格线
+# Txhyh就是被A变换过的新的网格线
+
 
 st.latex(r"A = " + bmatrix(A))
 
@@ -119,6 +148,7 @@ st.latex(
     + bmatrix(a2)
 )
 
+
 st.latex(r"\begin{vmatrix} A \end{vmatrix} = " + str(np.linalg.det(A)))
 square_x = np.array([0, 1, 1, 0])
 square_y = np.array([0, 0, 1, 1])
@@ -127,6 +157,7 @@ square_array = np.stack((square_x, square_y))
 fig.add_trace(
     go.Scatter(x=square_x, y=square_y, fill="toself", line_color="orange"), 1, 1
 )
+
 
 A_times_square_array = A @ square_array
 
@@ -152,6 +183,7 @@ fig.add_trace(
     1,
     2,
 )
+
 fig.update_xaxes(range=[-4, 4])
 fig.update_yaxes(range=[-4, 4])
 fig.update_layout(
